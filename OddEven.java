175108fee8c643ac9f17bbd1c55e76329364ef6f
Rahul9643 <krahul22172@gmail.com>
2025-12-28 21:59:17 +0530

    Update prompt message for user input

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter any number :- ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.print(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+    }
+}
