3489b0aeb8d6498a99db98ede17a19bd5b5ba339
Rahul9643 <krahul22172@gmail.com>
2025-12-29 22:26:10 +0530

    Implement loop for repeated prime number checks
    Added a loop to allow multiple prime number checks.

--- Prime number.java
+++ Prime number.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Primenumber{
     public static void main(String[] args){
+        while(true){
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter a number:-");
     int number = scanner.nextInt();
@@ -11,6 +12,11 @@ public class Primenumber{
         System.out.println(number + " is a not prime number");
         
     }
+            System.out.print("Do you want to check another number?(yes/no): ");
+            String choice = scanner.next();
+            if(choice.equalsIgnoreCase("no"));
+            break;
+        }        
     }
     public static boolean isPrime(int num){
         for(int i=2; i<=num/2; i++){
