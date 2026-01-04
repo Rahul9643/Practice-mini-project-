23910d1e61f12dcfb98ba62b0397ebb9474c587e
codeforge360 <krahul22172@gmail.com>
2026-01-04 13:24:21 +0530

    Add Java program to calculate factorial of a number

---
+++ Factorial find of any number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args){
+     
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        do{
+            int factorial = 1;
+            System.out.print("Enter any number:- ");
+            int number = sc.nextInt();
+            for(int i = 1; i<=number; i++){
+                factorial = factorial*i;
+            }
+            System.out.println("Factorial number is :- "+ factorial);
+            
+          System.out.println("another number(yes/no)") ;
+          choice = sc.next();
+          
+        }
+        while(choice.equalsIgnoreCase("yes"));
+        System.out.println("program ended");   
+        
+    }
+}
